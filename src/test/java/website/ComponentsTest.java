86abc529f000c7892cc0ca30bddf5084b7865ced
Istvan Viczian <viczian.istvan@gmail.com>
2022-06-15 16:52:37 +0200

    Wait

--- src/test/java/website/ComponentsTest.java
+++ src/test/java/website/ComponentsTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SeleniumTest
 @Slf4j
@@ -71,10 +72,56 @@ class ComponentsTest {
             var cells = row.findElements(By.tagName("td"));
             long id = Long.parseLong(cells.get(0).getText());
             String name = cells.get(1).getText();
-            int yearofBirth = Integer.parseInt(cells.get(2).getText());
-            var employee = new Employee(id, name, yearofBirth);
+            int yearOfBirth = Integer.parseInt(cells.get(2).getText());
+            var employee = new Employee(id, name, yearOfBirth);
             employees.add(employee);
         }
         return employees;
+    }
+
+    @Test
+    void testInputField(WebDriver driver) {
+        driver.get("http://127.0.0.1:5500/components/index.html");
+        var input = driver.findElement(By.name("text"));
+        input.sendKeys("hello input");
+
+        log.debug(input.getDomProperty("value"));
+    }
+
+    @Test
+    void testCheckbox(WebDriver driver) {
+        driver.get("http://127.0.0.1:5500/components/index.html");
+        var checkbox = driver.findElement(By.name("checkbox"));
+        // checkbox.click();
+        var label = driver.findElement(By.cssSelector("[for=checkbox1]"));
+        label.click();
+
+        assertTrue(checkbox.isSelected());
+
+        assertFalse(driver.findElement(By.name("disabled-checkbox")).isEnabled());
+    }
+
+    @Test
+    void testRadioButton(WebDriver driver) {
+        driver.get("http://127.0.0.1:5500/components/index.html");
+
+        driver.findElement(By.id("radiobtn1")).click();
+
+        var count = driver
+                .findElements(By.cssSelector("input[type=radio]"))
+                .stream()
+                .filter(we -> we.isSelected())
+                .count();
+        assertEquals(1, count);
+    }
+
+    @Test
+    void testSelect(WebDriver driver) {
+        driver.get("http://127.0.0.1:5500/components/index.html");
+
+        var select = new Select(driver.findElement(By.id("dropdown")));
+        select.selectByValue("option3");
+
+        assertEquals("Option 3", select.getFirstSelectedOption().getText());
     }
 }
